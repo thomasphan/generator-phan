6b77067aaa52b2ece2db75ef576821cf1394cc60
Thomas Phan <phant@intific.com>
2018-07-08 21:53:43 -0500

    More webpack configs

--- generators/app/templates/webpack.config.js
+++ generators/app/templates/webpack.config.js
@@ -1,14 +1,38 @@
 const merge = require('webpack-merge')
 const path = require('path')
 
+const rules = [
+  {
+    test: /\.(png|svg)$/,
+    use: ['file-loader']
+  },
+  {
+    test: /\.css$/,
+    use: ['style-loader', 'css-loader']
+  },
+  {
+    test: /\.tsx?$/,
+    use: ['ts-loader'],
+    exclude: /node_modules/,
+  },
+]
+
+const extensions = [
+  '.tsx',
+  '.ts',
+  '.js',
+]
+
 const output = {
   path: path.resolve(__dirname, './public/js'),
   publicPath: '/js/',
 }
 
 const config = {
+  module: { rules },
   output,
   mode: 'development',
+  resolve: { extensions },
 }
 
 const configs = [
